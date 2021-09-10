752965212276bbf041a509313ca3c13acfcd4964
Sean Macdonald <sean.r.macdonald@gmail.com>
2021-09-10 09:33:16 -0400

    we need restserver version for hooks

--- localdev.sh
+++ localdev.sh
@@ -20,18 +20,6 @@ docker run --name=$DOCKER_CONTAINER_NAME --rm \
   "${TUSD_CONTAINER_IMAGE_LOCATION}:$GIT_REF" \
   -s3-bucket="$ECS_BUCKET_NAME" \
   -s3-endpoint="$ECS_ENDPOINT" \
-  -s3-part-size="$FILE_CHUNK_SIZE"
-
-exit 0
-
-docker run --name=$DOCKER_CONTAINER_NAME --rm \
-  -e AWS_ACCESS_KEY_ID \
-  -e AWS_SECRET_ACCESS_KEY \
-  -e AWS_REGION \
-  --network=host \
-  "${TUSD_CONTAINER_IMAGE_LOCATION}:$GIT_REF" \
-  -s3-bucket="$ECS_BUCKET_NAME" \
-  -s3-endpoint="$ECS_ENDPOINT" \
   -s3-part-size="$FILE_CHUNK_SIZE" \
   -s3-object-prefix="$ECS_BUCKET_NAMESPACE" \
   -hooks-http=http://$RESTSERVER_URL:$RESTSERVER_PORT/v$RESTSERVER_VERSION/assets/tusMessage
